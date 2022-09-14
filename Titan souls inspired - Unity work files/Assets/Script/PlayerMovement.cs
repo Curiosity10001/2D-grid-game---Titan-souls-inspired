72ea19efaae4c019b7c2b111ede831432380fa75
Imane BOUROUYNE (MA/FR) <110111059+Curiosity10001@users.noreply.github.com>
2022-09-14 16:26:03 +0200

    player movement done , state machine for animations to do

--- Titan souls inspired - Unity work files/Assets/Script/PlayerMovement.cs
+++ Titan souls inspired - Unity work files/Assets/Script/PlayerMovement.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    
+    public Rigidbody2D _rgbdP;
+    public float _speed = 5f;
+    private Vector2 _direction;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        _direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+    private void FixedUpdate()
+    {
+        _rgbdP.velocity = _direction.normalized * _speed;
     }
 }
